206da957de6cd0358ae445ae83fbfd213ca2e98d
Darlo Miguel Ilagan <dmcilagan@gmail.com>
2021-03-20 07:42:36 +0800

    Create todo.js

---
+++ src/pages/todo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+// import { Link } from 'gatsby';
+import Sidebar from '../components/Sidebar';
+import config from '../../config';
+import Card from 'react-bootstrap/Card';
+
+import melawares from '../assets/images/melawares.png';
+import koppel from '../assets/images/koppel.png';
+import ltph from '../assets/images/ltph.png';
+import pearlpay from '../assets/images/pearlpay.png';
+import portfolio from '../assets/images/portfolio.png';
+import scud from '../assets/images/scud.png';
+import videoscribe from '../assets/images/videoscribe.png';
+
+const IndexPage = () => (
+  <Layout>
+   <iframe src='https://flask.io/x6uZp8ocJHls?embed=true' style='width: 100%; height: 500px;' frameborder='0' allowfullscreen='allowfullscreen'></iframe>
+  </Layout>
+);
+
+export default IndexPage;
